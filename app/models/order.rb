8c2f00f7c70dff699acc16207782d8cc6be95390
mariialauraa <mlauraifg@gmail.com>
2023-11-08 13:35:45 -0300

    Storefront 1.15 Model de Pedidos

---
+++ app/models/order.rb
@@ -0,0 +1,32 @@
+class Order < ApplicationRecord
+  DAYS_TO_DUE = 7 #constante
+
+  belongs_to :user
+  belongs_to :coupon, optional: true
+
+  validates :status, presence: true, on: :update
+  validates :subtotal, presence: true, numericality: { greater_than: 0 }
+  validates :total_amount, presence: true, numericality: { greater_than: 0 }
+  validates :payment_type, presence: true
+  #números de vezes da parcela
+  validates :installments, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  enum status: { processing_order: 1, processing_error: 2, waiting_payment: 3,
+                 payment_accepted: 4, payment_denied: 5, finished: 6 }
+
+  enum payment_type: { credit_card: 1, billet: 2 }
+
+  #chama o método antes da validação apenas durante a criação
+  before_validation :set_default_status, on: :create
+
+  #método da data de vencimento
+  def due_date
+    self.created_at + DAYS_TO_DUE.days
+  end
+
+  private
+
+  def set_default_status
+    self.status = :processing_order
+  end
+end
