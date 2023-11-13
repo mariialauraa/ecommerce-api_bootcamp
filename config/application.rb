4549fbc8e3fc709b81c711dff2ab4a2a4cc544f4
mariialauraa <mlauraifg@gmail.com>
2023-11-13 16:30:33 -0300

    Storefront 1.37 Instalação do Redis e do Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module EcommerceApi
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
     config.i18n.default_locale = :'pt-BR'
 
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -41,5 +40,7 @@ module EcommerceApi
     config.api_only = true
 
     config.autoload_paths += %W["#{config.root}/app/validators/"]
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
