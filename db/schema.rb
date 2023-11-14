dd925e9e3fc537cc485b0d88d5893c22cb9c24d8
mariialauraa <mlauraifg@gmail.com>
2023-11-14 10:46:07 -0300

    Storefront 2.6 Adicionando status aos Itens do Produto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_13_123843) do
+ActiveRecord::Schema.define(version: 2023_11_14_130000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2023_11_13_123843) do
     t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
